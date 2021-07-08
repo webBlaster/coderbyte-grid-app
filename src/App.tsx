ba5444bd128a61400e91f403d59628dc6104a1b2
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-07-08 15:01:51 +0100

    filtered data with shape properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import data from './data.json';
+import React from "react";
+import data from "./data.json";
 
 function App() {
-  const shapes = data.map((item) => {
-    return (<li>{item.shape}</li>)
+  //property arrays
+  let shapes = ["round", "rectangle", "triangle", "oval", "square"];
+  let colors = ["red", "blue", "green", "yellow", "grey", "ash"];
+
+  //initialize maps
+  let shapeMap = new Map();
+  let colorMap = new Map();
+
+  //set default map values
+  colors.forEach((item) => {
+    colorMap.set(item, true);
   });
-  return (
-    <ul className="App">
-      {shapes}
-    </ul>
-  );
+
+  shapes.forEach((item) => {
+    shapeMap.set(item, true);
+  });
+
+  console.log(shapeMap, colorMap);
+
+  let filteredData = data.filter((item) => {
+    return shapeMap.get(item.shape) && colorMap.get(item.color);
+  });
+
+  const shapeList = filteredData.map((item) => {
+    return (
+      <li key={item.id}>
+        {item.shape} and {item.color}{" "}
+      </li>
+    );
+  });
+  return <ul className="grid">{shapeList}</ul>;
 }
 
 export default App;
