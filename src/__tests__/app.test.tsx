2cca222622997bbd145dd57db659c7f1bc932f75
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-07-11 10:39:34 +0100

    installed mock library for redux store

---
+++ src/__tests__/app.test.tsx
@@ -0,0 +1,17 @@
+import App from "../app";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+const mockStore = configureStore([]);
+
+describe("check if components render without crashing", () => {
+  const store = mockStore({ auth: { isAuthenticated: true } });
+  it("check if app renders", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+});
--- src/__tests__/header.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Header from "../components/header";
-
-it("Check if Header renders", () => {
-  render(<Header />);
-});
